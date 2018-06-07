d20264afe13fe2b26f54969518385ea45d297873
Niko <nbessinas@plausa.com>
2018-06-07 14:39:03 -0400

    Added Basic Adjacency Matrix Class

---
+++ GraphStudy/AdjacencyMatrixGraph.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphStudy
+{
+    class AdjacencyMatrixGraph<T>
+    {
+        private List<List<T>> _adjacencyMatrix;
+
+        public List<List<T>> AdjacencyMatrix
+        {
+            get
+            {
+                return _adjacencyMatrix;
+            }
+
+            set
+            {
+                _adjacencyMatrix = value;
+            }
+        }
+
+        public AdjacencyMatrixGraph()
+        {
+            _adjacencyMatrix = new List<List<T>>();
+        }
+    }
+}
