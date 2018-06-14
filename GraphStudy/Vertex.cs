3fa6f84b5cfc0ffd94bfe9b836fc1ce4be0bbf69
Nizobe <oshasnan@yahoo.com>
2018-06-14 17:53:20 -0400

    add graph class

---
+++ GraphStudy/Vertex.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphStudy
+{
+    class Vertex<T>
+    {
+        T _data;
+
+        List<int> _neighbors;//stores hash codes of neighbors instead of neighbors themselves
+    }
+}
